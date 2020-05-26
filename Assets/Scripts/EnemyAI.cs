285b09ec7137f825c0d347ceeff8fd1d04051399
Fede <fedemc@gmail.com>
2020-05-26 18:10:08 -0300

    Lots of stuff done: - Gun on player, animation, vfx, hit detection. - Enemy model with animations and death functionality. - VFXs for shot hit

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,29 +1,78 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
+    [SerializeField] float attackRange = 0.5f;
+    [SerializeField] Animator animController;
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.stoppingDistance = attackRange;
+        animController = GetComponent<Animator>();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.75f);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 
     // Update is called once per frame
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if(distanceToTarget <= chaseRange)
+        
+        if(isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
-        }        
+            EngageTarget();
+        }
+        else if(distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+            //navMeshAgent.SetDestination(target.position);
+        }
+
+    }
+
+    private void EngageTarget()
+    {
+        bool isOnAttackRange = distanceToTarget <= navMeshAgent.stoppingDistance;
+
+        if (!isOnAttackRange)
+        {
+            ChaseTarget();
+        }
+        
+        if(isOnAttackRange)
+        {
+            AttackTarget();
+        }
+
+    }
+
+    private void ChaseTarget()
+    {
+        animController.SetBool("onAttackRange", false);
+        animController.SetTrigger("Move");
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        //Debug.Log(gameObject.name + " attacking target");
+        animController.SetBool("onAttackRange", true);
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float weaponRange = 100f;
+    [SerializeField] ParticleSystem muzzleVFX;
+    [SerializeField] GameObject shotHitVFX;
+    float damage=25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+            
+        }
+    }
+
+    private void Shoot()
+    {
+        anim.SetTrigger("Shoot");
+        muzzleVFX.Play();
+        ProcessRayCast();
+
+    }
+
+    private void ProcessRayCast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, weaponRange))
+        {
+            CreateHitImpact(hit);
+            EnemyHealth enemyHealth = hit.transform.gameObject.GetComponent<EnemyHealth>();
+            // If we hit enemy, reduce enemy health
+            if (enemyHealth)
+            {
+                enemyHealth.ReduceHealth(damage);
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject shotHit = Instantiate(shotHitVFX, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(shotHit, 1f);
+    }
+}
